c95bcf1a3ed81dcdf152f46dc20486d926f7a621
Denys Natalchuk <d.natalchuk@gmail.com>
2023-07-25 19:36:04 +0200

    fix

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -2,18 +2,20 @@
 
 set -ex
 
-cd ./src && docker build --tag scarf-test:latest .
+IMAGE_TAG="scarf_test:1.0.0"
+
+cd ./src && docker build --tag $IMAGE_TAG .
 [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 kind create cluster
 kind get clusters
 kubectl cluster-info --context kind-kind
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -sLO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-kind load docker-image scarf-test:latest --name kind
+kind load docker-image $IMAGE_TAG --name kind
 sleep 50
 docker exec kind-control-plane crictl images
 # kubectl apply -f ../site-deployment.yml
