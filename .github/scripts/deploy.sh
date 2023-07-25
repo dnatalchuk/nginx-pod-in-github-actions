ec4c765e59c843e6ee4d32e963270c3736878df8
Denys Natalchuk <d.natalchuk@gmail.com>
2023-07-25 19:41:54 +0200

    fix

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -18,16 +18,9 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kind load docker-image $IMAGE_TAG --name kind
 # docker exec kind-control-plane crictl images
 kubectl apply -f ../site-deployment.yml
-sleep 20
 kubectl apply -f ../site-service.yml
-sleep 20
-kubectl get pods -n default
-sleep 10
 kubectl get pods -n default
 hello_world_pod_name=$(kubectl get pods -n default --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
-kubectl describe pod $hello_world_pod_name -n default
-
-# hello_world_pod_name=$(kubectl get pods -n default --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
-# kubectl exec -it -n default $hello_world_pod_name -- curl localhost:8080
-# sleep 5
+kubectl exec -it -n default $hello_world_pod_name -- curl localhost:8080
+sleep 5
 # kind delete cluster
