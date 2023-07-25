52d4da496d4ca805340199597223d8075dcfdec8
Denys Natalchuk <d.natalchuk@gmail.com>
2023-07-25 19:39:00 +0200

    fix

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -16,16 +16,16 @@ curl -sLO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kind load docker-image $IMAGE_TAG --name kind
-sleep 50
-docker exec kind-control-plane crictl images
-# kubectl apply -f ../site-deployment.yml
-# kubectl apply -f ../site-service.yml
-# sleep 10
-# kubectl get pods -n default
-# sleep 10
-# kubectl get pods -n default
-# hello_world_pod_name=$(kubectl get pods -n default --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
-# kubectl describe pod $hello_world_pod_name -n default
+# docker exec kind-control-plane crictl images
+kubectl apply -f ../site-deployment.yml
+sleep 20
+kubectl apply -f ../site-service.yml
+sleep 20
+kubectl get pods -n default
+sleep 10
+kubectl get pods -n default
+hello_world_pod_name=$(kubectl get pods -n default --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+kubectl describe pod $hello_world_pod_name -n default
 
 # hello_world_pod_name=$(kubectl get pods -n default --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
 # kubectl exec -it -n default $hello_world_pod_name -- curl localhost:8080
