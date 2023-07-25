0be3cfde01d6242013b71fed2b5f785221599bbb
Denys Natalchuk <d.natalchuk@gmail.com>
2023-07-25 19:44:35 +0200

    fix

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -5,7 +5,7 @@ set -ex
 IMAGE_TAG="scarf_test:1.0.0"
 
 cd ./src && docker build --tag $IMAGE_TAG .
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+[ $(uname -m) = x86_64 ] && curl -sLo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 kind create cluster
@@ -16,11 +16,12 @@ curl -sLO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kind load docker-image $IMAGE_TAG --name kind
-# docker exec kind-control-plane crictl images
 kubectl apply -f ../site-deployment.yml
+slee 20
 kubectl apply -f ../site-service.yml
+slee 20
 kubectl get pods -n default
 hello_world_pod_name=$(kubectl get pods -n default --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
-kubectl exec -it -n default $hello_world_pod_name -- curl localhost:8080
+kubectl exec -n default $hello_world_pod_name -- curl localhost:8080
 sleep 5
 # kind delete cluster
