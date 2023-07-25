49fb9369ba72442371f75c5359be0a3f8a232a72
Denys Natalchuk <d.natalchuk@gmail.com>
2023-07-25 18:42:12 +0200

    fix

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-
+docker  --version
 [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
